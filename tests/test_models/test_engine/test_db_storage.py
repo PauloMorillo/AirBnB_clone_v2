983d40982b22b70ff85488c2b69b20f53c48f49d
PauloMorillo <pauloan@hotmail.com>
2019-12-20 15:08:48 +0000

    create test for dbstorage

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""test for file storage"""
+import unittest
+import pep8
+import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    '''this will test the FileStorage'''
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = db_storage()
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
+
+
+if __name__ == "__main__":
+    unittest.main()
