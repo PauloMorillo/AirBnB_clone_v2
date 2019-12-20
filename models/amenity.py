68c3cc9fc66fcf0a0efa5a86800eee440cff93ac
LauraPeraltaV85 <lalaperalta85@gmail.com>
2019-12-20 22:25:48 +0000

    [upload again

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
-from models.place import *
+import models.place
+
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
@@ -15,7 +16,7 @@ class Amenity(BaseModel, Base):
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place",
-                                       secondary=place_amenity,
+                                       secondary=models.place.place_amenity,
                                        backref="Amenity")
 
     else:
