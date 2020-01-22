9a21289dca64eb73e683301bd480244601d01fe8
PauloMorillo <pauloan@hotmail.com>
2020-01-22 14:20:34 +0000

    fixed template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,23 +2,33 @@
 """ This script starts a flask application to storage all data in mysql"""
 
 
-from flask import Flask, request_template
+from flask import Flask, render_template
 from models import storage
 from models import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
-def handleclose():
+def handleclose(self):
     """ method to handle close session """
     storage.close()
+
 
 @app.route('/states_list')
 def showstates():
     """ route to tempplate for showing sstates """
-    templatedata = {'States': storage.all(State)}
-    return request_template("7-states_list.html", **templatedata)
+    objects = storage.all(State)
+    # print(objects)
+    states = dict()
+    for key in objects.items():
+        a = key[1]
+        if type(a) == State:
+            states[a.id] = a.name
+    # print(states)
+    templatedata = {'States': states}
+    return render_template("7-states_list.html", **templatedata)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
